b41c4b71ca349238f2b20d64cef0da4d29d3d324
praandika <praandikayoga@gmail.com>
2026-03-25 06:28:41 +0800

    Create Auth Foundation - VerifyToken in lib\auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey"
 
+// Buat Token
 export function generateToken(user) {
     return jwt.sign(
         {
@@ -16,6 +17,18 @@ export function generateToken(user) {
     )
 }
 
-export function verifyToken(token) {
-    return jwt.verify(token, JWT_SECRET)
+// Protect API = Verify token (akses API)
+export function verifyToken(request) {
+    try {
+        const authHeader = request.headers.get("authorization")
+
+        if (!authHeader) return null
+
+        const token = authHeader.split(" ")[1]
+
+        return jwt.verify(token, JWT_SECRET)
+
+    } catch (error) {
+        return null
+    }
 }
